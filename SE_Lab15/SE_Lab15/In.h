8c47f59b27713e6ddcb6155e24936cf9e1024aa9
Wandskape <name.silv@yandex.by>
2023-10-18 15:40:43 +0300

    creating lexical analyzer

---
+++ SE_Lab15/SE_Lab15/In.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+#define IN_MAX_LEN_TEXT 1024*1024 // максимальный размер исходного кода
+#define IN_CODE_ENDL '\n' // символ конца строки
+
+// таблица проверки входной информации, индекс = код (Windows-1251) символа
+// значения IN::F - запрещенный символ, IN::F = разрешенный символ, IN::F - игнорировать (не вводить),
+// если 0 <= значение < 256 - то вводится данное значение
+
+#define MAX_LEN_LINE 1000
+namespace In
+{
+	void dell_in(char* str, int index);
+	struct IN
+	{
+		enum
+		{
+			T = 1024, // допустимый символ
+			F = 2048, // недопустимый
+			I = 4096 // игнорировать, иначе изменить
+		};
+
+		int size, // размер исходного кода
+			lines, // количество строк
+			ignore; // количество проигнорированных символов
+
+		unsigned char* text; // исходный код(Windows-1251);
+
+		int code[256] =
+		{
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, '|', IN::F, IN::F, IN::F, IN::F, IN::F, // 0 - 15
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 16
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 32
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 48
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 64
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 80
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 96
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 112
+
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 128
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 144
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 160
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 176
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 192
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 208
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 224
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F
+		}; // таблица проверки: T, F, I новое значение
+
+		IN()
+		{
+				code[78] = code[110] = code[32] = code[48] = code[50] = code[52] = code[65] = code[67] = code[69] = code[72] = code[73] = code[75] = code[76] = code[77] = code[79] = code[82] = code[83] = code[85] =
+				code[97] = code[99] = code[101] = code[104] = code[105] = code[107] = code[108] = code[109] = code[111] = code[114] = code[115] = code[117] =
+				code[192] = code[197] = code[200] = code[201] = code[202] = code[203] = code[204] = code[205] = code[206] = code[208] = code[209] = code[211] = code[215] = code[224] = code[229] = code[232] = code[233]  = code[234] = code[235] = code[236] = code[237] =
+				code[238] = code[240] = code[241] = code[243] = code[247] = IN::T;
+
+
+			code[66] = '-';
+			code[88] = IN::I;
+		}
+	};
+
+	IN getin(wchar_t infile[]);
+}
